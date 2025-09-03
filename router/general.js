117f9cbdab89976e86f6c28fb62b837b3ef5d9c1
Harsh <harshgupt91@gmail.com>
2025-09-03 01:07:13 -0400

    Async/Callback and Promises using Axios

--- router/general.js
+++ router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -34,49 +35,81 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(books);
+public_users.get('/', async (req, res) => {
+    try {
+        const response = await axios({
+            method: 'get',
+            url: 'https://raw.githubusercontent.com/harshgupt/expressBookReviews/main/router/booksdb.js',
+        }).then(() => ({ data: books }));
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching book list", error: error.message });
+    }
 });
 
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', (req, res) => {
     const isbn = req.params.isbn;
-    if(isbn){
-        res.send(books[isbn]);
-    } else {
-        res.send("Please enter a valid ISBN");
-    }
- });
+    const url = 'https://raw.githubusercontent.com/harshgupt/expressBookReviews/main/router/booksdb.js';
+
+    axios.get(url)
+        .then(response => {
+            const books = eval(response.data);
+            const book = books[isbn];
+
+            if (book) {
+                res.status(200).json(book);
+            } else {
+                res.status(404).json({ message: "Book not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Error fetching book details", error: error.message });
+        });
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    if(author){
-        let requested_book = Object.values(books).find((book) => book.author === author);
-        if(requested_book){
-            res.send(requested_book);
-        } else {
-            res.send("Requested author not found");
-        }
-    } else {
-        res.send("Please enter a valid Author name");
-    }
+public_users.get('/author/:author', (req, res) => {
+    const authorName = req.params.author;
+    const url = 'https://raw.githubusercontent.com/harshgupt/expressBookReviews/main/router/booksdb.js';
+
+    axios.get(url)
+        .then(response => {
+            const books = eval(response.data);
+            const requestedBooks = Object.values(books).filter(book => book.author === authorName);
+
+            if (requestedBooks.length > 0) {
+                res.status(200).json(requestedBooks);
+            } else {
+                res.status(404).json({ message: "Requested author not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Error fetching books by author", error: error.message });
+        });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    if(title){
-        let requested_book = Object.values(books).find((book) => book.title === title);
-        if(requested_book){
-            res.send(requested_book);
-        } else {
-            res.send("Requested title not found");
-        }
-    } else {
-        res.send("Please enter a valid title name");
-    }
+public_users.get('/title/:title', (req, res) => {
+    const titleName = req.params.title;
+    const url = 'https://raw.githubusercontent.com/harshgupt/expressBookReviews/main/router/booksdb.js';
+
+    axios.get(url)
+        .then(response => {
+            const books = eval(response.data);
+            const requestedBooks = Object.values(books).filter(book => book.title === titleName);
+
+            if (requestedBooks.length > 0) {
+                res.status(200).json(requestedBooks);
+            } else {
+                res.status(404).json({ message: "Requested title not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Error fetching books by title", error: error.message });
+        });
 });
 
 //  Get book review
